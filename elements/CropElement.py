7f5f2767fa2cb999ace54d5456dcf9a8cf61c888
Колесников Дмитрий Андреевич <kolesnikovda@digtp.com>
2024-03-05 21:57:53 +0300

    oop inserted

--- elements/CropElement.py
+++ elements/CropElement.py
@@ -1,14 +1,20 @@
+import numpy as np
+import cv2
+
+
 class CropElement:
     # Класс, содержаций информацию о конкретном кропе
     def __init__(
         self,
         source_image: np.ndarray,
+        source_image_resized: np.ndarray,
         crop: np.ndarray,
         number_of_crop: int,
         x_start: int,
         y_start: int,
     ) -> None:
-        self.source_image = source_image  # Исходное изображение (ресайзнутое до кратного размера кропам)
+        self.source_image = source_image  # Исходное изображение 
+        self.source_image_resized = source_image_resized  # Исходное изображение (ресайзнутое до кратного размера кропам)
         self.crop = crop  # Конкретный кроп 
         self.number_of_crop = number_of_crop  # Номер кропа по порядку слева направо сверху вниз
         self.x_start = x_start  # Координата верхнего левого угла Х
@@ -19,8 +25,8 @@ class CropElement:
         self.detected_xyxy = None  # Список списков с координатами xyxy боксов
         self.detected_masks = None # Список np массивов с масками в случае yolo-seg
         # Уточненные координаты согласноинформации о полодении кропа
-        self.detected_xyxy_real = None  # Список списков с координатами xyxy боксов в значениях от source_image
-        self.detected_masks_real = None # Список np массивов с масками в случае yolo-seg размером как source_image
+        self.detected_xyxy_real = None  # Список списков с координатами xyxy боксов в значениях от source_image_resized
+        self.detected_masks_real = None # Список np массивов с масками в случае yolo-seg размером как source_image_resized
 
     def calculate_inference(self, model, imgsz=640, conf=0.35, iou=0.7, segment=False):
         # Perform inference
@@ -40,9 +46,7 @@ class CropElement:
         # Get the mask confidence scores
         self.detected_conf = pred.boxes.conf.cpu().numpy()
 
-        num_objects = len(classes)
-
-        if segment:
+        if segment and len(self.detected_cls) != 0:
             # Get the masks
             self.detected_masks = pred.masks.data.cpu().numpy()
 
@@ -52,8 +56,8 @@ class CropElement:
         x_start_global = self.x_start  # Global X coordinate of the crop
         y_start_global = self.y_start  # Global Y coordinate of the crop
 
-        self.detected_xyxy_real = []  # List of lists with xyxy box coordinates in the values ​​of the source_image
-        self.detected_masks_real = []  # List of np arrays with masks in case of yolo-seg sized as source_image
+        self.detected_xyxy_real = []  # List of lists with xyxy box coordinates in the values ​​of the source_image_resized
+        self.detected_masks_real = []  # List of np arrays with masks in case of yolo-seg sized as source_image_resized
 
         for bbox in self.detected_xyxy:
             # Calculate real box coordinates based on the position information of the crop
@@ -68,12 +72,13 @@ class CropElement:
             self.detected_masks_real = []
             for mask in self.detected_masks:
                 # Create a black image with the same size as the source image
-                black_image = np.zeros_like(self.source_image)
+                black_image = np.zeros((self.source_image_resized.shape[0], self.source_image_resized.shape[1]))
                 mask_resized = cv2.resize(np.array(mask).copy(), (self.crop.shape[1], self.crop.shape[0]),
                                           interpolation=cv2.INTER_NEAREST)
 
                 # Place the mask in the correct position on the black image
-                black_image[y_start_global:y_start_global+img.shape[0], x_start_global:x_start_global+img.shape[1]] = mask_resized
+                black_image[y_start_global:y_start_global+self.crop.shape[0],
+                            x_start_global:x_start_global+self.crop.shape[1]] = mask_resized
 
                 # Append the masked image to the list of detected_masks_real
                 self.detected_masks_real.append(black_image)
--- nodes/MakeCropsDetectThem.py
+++ nodes/MakeCropsDetectThem.py
@@ -9,14 +9,14 @@ from elements.CropElement import CropElement
 class MakeCropsDetectThem:
     # Класс, реализующий нарезку на кропы и прогон через сеть
     def __init__(
-        model_path,
+        self,
         image: np.ndarray,
+        model_path='yolov8m.pt',
         imgsz=640, conf=0.5, iou=0.7, 
         segment=False, 
         shape_x=700, shape_y=700,
         overlap_x=25, overlap_y=25,
     ) -> None:
-        
         self.model = YOLO(model_path)  # Load the model from the specified path
         self.image = image  # Input image
         self.imgsz = imgsz  # Size of the input image for inference
@@ -29,5 +29,91 @@ class MakeCropsDetectThem:
         self.overlap_y = overlap_y  # Percentage of overlap along the y-axis
         self.crops = []  # List to store the CropElement objects
 
+
+        self.crops = self.get_crops_xy(self.image, shape_x=self.shape_x, shape_y=self.shape_y,
+                 overlap_x=self.overlap_x, overlap_y=self.overlap_y, show=False, return_crop_elements=True)
+        self._detect_objects()
+
+
         
-    
+    def get_crops_xy(self, image_full, shape_x: int, shape_y: int,
+                 overlap_x=15, overlap_y=15, show=False, return_crop_elements=False):
+        """Preprocessing of the image. Generating crops with overlapping.
+
+        Args:
+            image_full (array): numpy array of an RGB image
+            
+            shape_x (int): size of the crop in the x-coordinate
+            
+            shape_y (int): size of the crop in the y-coordinate
+            
+            overlap_x (float, optional): Percentage of overlap along the x-axis
+                    (how much subsequent crops borrow information from previous ones)
+
+            overlap_y (float, optional): Percentage of overlap along the y-axis
+                    (how much subsequent crops borrow information from previous ones)
+
+            show (bool): enables the mode to display images using plt.imshow
+
+        """    
+        cross_koef_x = 1 - (overlap_x / 100)
+        cross_koef_y = 1 - (overlap_y / 100)
+
+        data_all_crops = []
+
+        y_steps = int((image_full.shape[0] - shape_y) / (shape_y * cross_koef_y)) + 1
+        x_steps = int((image_full.shape[1] - shape_x) / (shape_x * cross_koef_x)) + 1
+
+        y_new = round((y_steps-1) * (shape_y * cross_koef_y) + shape_y)
+        x_new = round((x_steps-1) * (shape_x * cross_koef_x) + shape_x)
+        image_innitial = image_full.copy()
+        image_full = cv2.resize(image_full, (x_new, y_new))
+
+        if show:
+            plt.figure(figsize=[x_steps*0.9, y_steps*0.9])
+
+        count = 0
+        for i in range(y_steps):
+            for j in range(x_steps):
+                x_start = int(shape_x * j * cross_koef_x)
+                y_start = int(shape_y * i * cross_koef_y)
+
+                # Check for residuals
+                if x_start + shape_x > image_full.shape[1]:
+                    print('Error in generating crops along the x-axis')
+                    continue
+                if y_start + shape_y > image_full.shape[0]:
+                    print('Error in generating crops along the y-axis')
+                    continue
+
+                im_temp = image_full[y_start:y_start + shape_y, x_start:x_start + shape_x]
+
+                # Display the result:
+                if show:
+                    plt.subplot(y_steps, x_steps, i * x_steps + j + 1)
+                    plt.imshow(im_temp)
+                    plt.axis('off')
+                count += 1
+
+                if return_crop_elements:
+                    data_all_crops.append(CropElement(
+                                            source_image=image_innitial,
+                                            source_image_resized=image_full,
+                                            crop=im_temp,
+                                            number_of_crop=count,
+                                            x_start=x_start,
+                                            y_start=y_start,
+                    ))
+                else:
+                    data_all_crops.append(im_temp)
+        if show:
+            plt.show()
+            print('Number of generated images:', count)
+
+        return data_all_crops
+
+
+    def _detect_objects(self):
+        for crop in self.crops:
+            crop.calculate_inference(self.model, imgsz=self.imgsz, conf=self.conf, iou=self.iou, segment=self.segment)
+            crop.calculate_real_values()
